e957baea710616be6daa95728cdb785464339526
blackCracker <fogxcfg@gmail.com>
2021-08-19 17:54:57 +0530

    develop frontend until check forms

---
+++ about.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>::: Clothes Store :::</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/font-awesome.min.css" />
+    <link rel="stylesheet" href="css/animate.css" />
+
+    <link rel="stylesheet" href="css/style.css" />
+
+    <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript"
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCZXJBVDf7R4JqmSpopVPoduIGWx1IwpBM"></script>
+    <script type="text/javascript" src="js/plugins.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+    <div class="svg-wrap">
+        <svg width="64" height="64" viewBox="0 0 64 64">
+            <path id="arrow-left"
+                d="M26.667 10.667q1.104 0 1.885 0.781t0.781 1.885q0 1.125-0.792 1.896l-14.104 14.104h41.563q1.104 0 1.885 0.781t0.781 1.885-0.781 1.885-1.885 0.781h-41.563l14.104 14.104q0.792 0.771 0.792 1.896 0 1.104-0.781 1.885t-1.885 0.781q-1.125 0-1.896-0.771l-18.667-18.667q-0.771-0.813-0.771-1.896t0.771-1.896l18.667-18.667q0.792-0.771 1.896-0.771z">
+            </path>
+        </svg>
+
+        <svg width="64" height="64" viewBox="0 0 64 64">
+            <path id="arrow-right"
+                d="M37.333 10.667q1.125 0 1.896 0.771l18.667 18.667q0.771 0.771 0.771 1.896t-0.771 1.896l-18.667 18.667q-0.771 0.771-1.896 0.771-1.146 0-1.906-0.76t-0.76-1.906q0-1.125 0.771-1.896l14.125-14.104h-41.563q-1.104 0-1.885-0.781t-0.781-1.885 0.781-1.885 1.885-0.781h41.563l-14.125-14.104q-0.771-0.771-0.771-1.896 0-1.146 0.76-1.906t1.906-0.76z">
+            </path>
+        </svg>
+    </div>
+
+
+    <!-- MAIN CONTENT -->
+
+    <div class="container-fluid">
+        <!-- ABOUT -->
+        <section id="about" class="light">
+            <header class="title">
+                <h2>About <span>Us</span></h2>
+                <p>Lorem ipsum Esse esse cillum nisi cillum deserunt tempor ut pariatur qui officia. Lorem ipsum Magna
+                    eu irure sint <strong>occaecat</strong> cupidatat dolore minim irure cillum.</p>
+            </header>
+
+            <div class="container">
+                <div class="row table-row">
+                    <div class="col-sm-6 hidden-xs">
+                        <div class="section-content">
+                            <div class="big-image" style="background-image:url(images/1.png)"></div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6">
+                        <div class="section-content">
+                            <div class="about-content left animated" data-animate="fadeInLeft">
+                                <div class="about-icon"><i class="fa fa-code"></i></div>
+                                <div class="about-detail">
+                                    <h4>Clean Code</h4>
+                                    <p>Lorem ipsum Ad nisi dolore ut sit do dolor commodo sint ullamco. Lorem ipsum
+                                        Magna enim eu irure sit eiusmod fugiat et. Lorem ipsum Consequat in cupidatat
+                                        cupidatat non in non veniam dolore.</p>
+                                </div>
+                            </div>
+
+                            <div class="about-content left animated" data-animate="fadeInLeft">
+                                <div class="about-icon"><i class="fa fa-desktop"></i></div>
+                                <div class="about-detail">
+                                    <h4>Fully Responsive</h4>
+                                    <p>Lorem ipsum Ex labore dolor magna in proident ad reprehenderit consequat irure
+                                        nostrud quis. Lorem ipsum Veniam velit non cupidatat nulla et proident.</p>
+                                </div>
+                            </div>
+
+                            <div class="about-content left animated" data-animate="fadeInLeft">
+                                <div class="about-icon"><i class="fa fa-cube"></i></div>
+                                <div class="about-detail">
+                                    <h4>Multipurpose Design</h4>
+                                    <p>Lorem ipsum Ea quis sint sint aliquip qui exercitation commodo non cupidatat do
+                                        reprehenderit dolor. Lorem ipsum Non et minim incididunt sed labore in amet ea
+                                        est pariatur.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> <!-- /.row table-row -->
+            </div> <!-- /.container -->
+        </section>
+
+
+        <!-- TEAM -->
+
+        <section id="team" class="light">
+            <header class="title">
+                <h2>Creative <span>Team</span></h2>
+                <p>Lorem ipsum Dolor et reprehenderit laborum deserunt aliquip incididunt tempor id non. Lorem ipsum
+                    Fugiat anim labore cupidatat consequat non in sit dolore anim anim consequat nisi.</p>
+            </header>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 col-sm-6 text-center">
+                        <div class="wrap animated" data-animate="fadeInDown">
+                            <div class="img-team">
+                                <img src="images/team-1.png" alt="" class="img-circle">
+                            </div>
+
+                            <h3>John Doe</h3>
+                            <h5>Creative Director</h5>
+
+                            <p>Lorem ipsum Proident incididunt sint dolor cupidatat pariatur dolore magna pariatur in ea
+                                aliqua.</p>
+
+                            <div class="team-social">
+                                <ul class="list-inline social-list">
+                                    <li><a href="#" class="fa fa-twitter"></a></li>
+                                    <li><a href="#" class="fa fa-linkedin"></a></li>
+                                    <li><a href="#" class="fa fa-facebook"></a></li>
+                                    <li><a href="#" class="fa fa-google-plus"></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 col-sm-6 text-center">
+                        <div class="wrap animated" data-animate="fadeInDown">
+                            <div class="img-team">
+                                <img src="images/team-2.png" alt="" class="img-circle">
+                            </div>
+
+                            <h3>Ellie Morgan</h3>
+                            <h5>Designer</h5>
+
+                            <p>Lorem ipsum Proident incididunt sint dolor cupidatat pariatur dolore magna pariatur in ea
+                                aliqua.</p>
+
+                            <div class="team-social">
+                                <ul class="list-inline social-list">
+                                    <li><a href="#" class="fa fa-twitter"></a></li>
+                                    <li><a href="#" class="fa fa-linkedin"></a></li>
+                                    <li><a href="#" class="fa fa-facebook"></a></li>
+                                    <li><a href="#" class="fa fa-google-plus"></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 col-sm-6 text-center">
+                        <div class="wrap animated" data-animate="fadeInDown">
+                            <div class="img-team">
+                                <img src="images/team-3.png" alt="" class="img-circle">
+                            </div>
+
+                            <h3>Elche</h3>
+                            <h5>Developer</h5>
+
+                            <p>Lorem ipsum Proident incididunt sint dolor cupidatat pariatur dolore magna pariatur in ea
+                                aliqua.</p>
+
+                            <div class="team-social">
+                                <ul class="list-inline social-list">
+                                    <li><a href="#" class="fa fa-twitter"></a></li>
+                                    <li><a href="#" class="fa fa-linkedin"></a></li>
+                                    <li><a href="#" class="fa fa-facebook"></a></li>
+                                    <li><a href="#" class="fa fa-google-plus"></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 col-sm-6 text-center">
+                        <div class="wrap animated" data-animate="fadeInDown">
+                            <div class="img-team">
+                                <img src="images/team-4.png" alt="" class="img-circle">
+                            </div>
+
+                            <h3>JF Thiago</h3>
+                            <h5>Commerce</h5>
+
+                            <p>Lorem ipsum Proident incididunt sint dolor cupidatat pariatur dolore magna pariatur in ea
+                                aliqua.</p>
+
+                            <div class="team-social">
+                                <ul class="list-inline social-list">
+                                    <li><a href="#" class="fa fa-twitter"></a></li>
+                                    <li><a href="#" class="fa fa-linkedin"></a></li>
+                                    <li><a href="#" class="fa fa-facebook"></a></li>
+                                    <li><a href="#" class="fa fa-google-plus"></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- /.container -->
+        </section>
+    </div><!-- /.container-fluid -->
+
+    <!-- SCRIPT -->
+    <script type="text/javascript" src="js/main.js"></script>
+</body>
+
+</html>
---
+++ checkout_user_form.php
@@ -0,0 +1,124 @@
+<html lang="en">
+<head>
+  
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="./css/checkout_form.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>::: Brand Name - Checkout :::</title>
+</head>
+<body>
+    
+	<div class="section">
+		<div class="container">
+			<div class="row full-height justify-content-center">
+				<div class="col-12 text-center align-self-center py-5">
+					<div class="section pb-5 pt-5 pt-sm-2 text-center">
+						<h6 class="mb-0 pb-3"><span>User Form </span><span>CheckOut Form</span></h6>
+			          	<input class="checkbox" type="checkbox" id="reg-log" name="reg-log"/>
+			          	<label for="reg-log"></label>
+						<div class="card-3d-wrap mx-auto">
+							<div class="card-3d-wrapper">
+								<div class="card-front">
+									<div class="center-wrap">
+										<div class="section text-center">
+											<!-- icon -->
+										<i class="bi bi-person-lines-fill"></i>
+                                            <!-- name -->
+											<div class="form-group">
+												<input type="text" name="name" class="form-style" placeholder="Full Name" id="name" autocomplete="off">
+											</div>
+                                            <!-- email -->
+											<div class="form-group mt-2">
+												<input type="email" name="email" class="form-style" placeholder=" E-mail" id="email" autocomplete="off">
+											</div>
+                                            <!-- mobile -->
+											<div class="form-group">
+													<input type="text" name="mobile" class="form-style" placeholder="Mobile Number" id="mobile" autocomplete="off">
+											</div>
+                                            <!-- product ID -->
+											<div class="form-group">
+												<input type="text" name="product_id" class="form-style" placeholder="product Id" id="product_id" autocomplete="off">
+											</div>
+                                            <!-- select size -->
+                                            <select class="dd_size  form-style" name="sizes" id="sizes">
+                                             	<option value="s">Select Size</option>
+                                             	<option value="s">S</option>
+                                             	<option value="m">M</option>
+                                             	<option value="l">L</option>
+                                             	<option value="xl">XL</option>
+                                            </select>
+                                            <!-- quantity -->
+											<div class="form-group">
+												<input type="text" name="quantity" class="form-style" placeholder="quantity" id="quantity" autocomplete="off">
+												
+											</div>
+                                            <!-- address -->
+											<div class="form-group">
+												<textarea type="text" name="address" class="form-style" placeholder="address" id="address" autocomplete="off"></textarea>
+											</div>
+                                            <!-- btn -->
+											<a href="#" class="btn mt-5">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+                                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                                <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
+                                                </svg>
+                                            </a>
+				      					</div>
+			      					</div>
+			      				</div>
+								<div class="card-back">
+									<div class="center-wrap">
+										<div class="section text-center">
+										<i class="bi bi-credit-card-2-front-fill"></i>
+											<!-- name -->
+											<div class="form-group">
+												<input type="text" name="name" class="form-style" placeholder="Full Name" id="name" autocomplete="off">
+												<i class="input-icon uil uil-at"></i>
+											</div>
+                                            <!-- email -->
+											<div class="form-group mt-2">
+												<input type="email" name="email" class="form-style" placeholder=" E-mail" id="email" autocomplete="off">
+											</div>
+                                            <!-- mobile -->
+											<div class="form-group">
+												<input type="text" name="mobile" class="form-style" placeholder="Mobile Number" id="mobile" autocomplete="off">
+											</div>
+											 <!-- date of birth -->
+											 <div class="form-group">
+											 	<p class="">Date Of Birth</p>
+												<input type="date" name="dob" class="form-style" placeholder="Date Of birth" id="dob" autocomplete="off">
+											</div>
+											<!-- card number -->
+											<div class="form-group">
+												<input type="text" name="card_no" class="form-style" placeholder="Card Number" id="card_no" autocomplete="off">
+											</div>
+											<!-- card cvc -->
+											<div class="form-group">
+												<input type="text" name="card_cvc" class="form-style" placeholder="Card CVC" id="card_cvc" autocomplete="off">
+											</div>
+											<!-- card expire date -->
+											<div class="form-group">
+											 	<p class="">Card Expire Date</p>
+												<input type="date" name="ex_date" class="form-style" placeholder="Expire Date" id="ex_date" autocomplete="off">
+											</div>
+                                             <!-- btn -->
+                                             <a href="#" class="btn mt-10">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+                                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                                <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
+                                                </svg>
+                                            </a>
+				      					</div>
+			      					</div>
+			      				</div>
+			      			</div>
+			      		</div>
+			      	</div>
+		      	</div>
+	      	</div>
+	    </div>
+	</div>
+</body>
+</html>
